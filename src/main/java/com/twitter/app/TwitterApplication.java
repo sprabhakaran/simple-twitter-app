f0d6d7e5e76a31bcfbdb7e42d610cf675e6fb5f9
sprabhakaran <hellospk7@gmail.com>
2021-06-11 21:39:56 +0530

    first commit

---
+++ src/main/java/com/twitter/app/TwitterApplication.java
@@ -0,0 +1,21 @@
+package com.twitter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class TwitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterApplication.class, args);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getString() {
+        return "Hello I;m String";
+    }
+
+}
