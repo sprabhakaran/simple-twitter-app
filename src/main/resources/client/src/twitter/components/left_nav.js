0d3583d51cd0d21523c1996b539fc6a59428ae69
sprabhakaran <hellospk7@gmail.com>
2021-07-02 22:34:13 +0530

    ui improved

--- src/main/resources/client/src/twitter/components/left_nav.js
+++ src/main/resources/client/src/twitter/components/left_nav.js
@@ -6,19 +6,47 @@ export default class LeftNavigation extends Component {
         return (
             <div className={"t-nav-left"}>
                 <div>
-                    <div><img src="https://image.flaticon.com/icons/png/512/733/733579.png" alt="logo" height={"50"}/>
-                    </div>
                     <div>
-
+                        <img src="https://image.flaticon.com/icons/png/512/733/733579.png" alt="logo" height={"50"}/>
                     </div>
-                    <div class="t-nav-footer">
+                    <div className={"t-action-holder"}>
+                        <div className={"t-home"}>
+                            <img src="https://img.icons8.com/office/2x/home.png" alt="home" className={"ui icon image"} height={"20"}/>
+                            <div>Home</div>
+                        </div>
+                        <div className={"t-explore"}>
+                            <img src="https://img.icons8.com/ios-glyphs/2x/hashtag-large.png" alt="explore" className={"ui icon image"} height={"20"}/>
+                            <div>Explore</div>
+                        </div>
+                        <div className={"t-notification"}>
+                            <img src="https://img.icons8.com/ios/2x/appointment-reminders.png" alt="notification" className={"ui icon image"} height={"20"}/>
+                            <div>Notifications</div>
+                        </div>
+                        <div className={"t-msg"}>
+                            <img src="https://img.icons8.com/fluent/2x/message-preview.png" alt="message" className={"ui icon image"} height={"20"}/>
+                            <div>Messages</div>
+                        </div>
+                        <div className={"t-bookmarks"}>
+                            <img src="https://img.icons8.com/ultraviolet/2x/bookmark-ribbon.png" alt="bookmarks" className={"ui icon image"} height={"20"}/>
+                            <div>Bookmarks</div>
+                        </div>
+                        <div className={"t-list"}>
+                            <img src="https://img.icons8.com/color/2x/list.png" alt="list" className={"ui icon image"} height={"20"}/>
+                            <div>List</div>
+                        </div>
+                        <div className={"t-profile"}>
+                            <img src="https://img.icons8.com/color/2x/test-account.png" alt="profile" className={"ui icon image"} height={"20"}/>
+                            <div>Profile</div>
+                        </div>
+                    </div>
+                    {/*<div class="t-nav-footer">
                         <div><img src="https://img.icons8.com/dotty/2x/user.png" className={"t-image"}/></div>
                         <div className={"t-prof-names"}>
                             <div>Prabhakaran S</div>
                             <div>@sprabhakaran</div>
                         </div>
 
-                    </div>
+                    </div>*/}
                 </div>
             </div>
         )
--- src/main/resources/client/src/twitter/components/main.js
+++ src/main/resources/client/src/twitter/components/main.js
@@ -3,7 +3,15 @@ import React, {Component} from "react";
 export default class Main extends Component {
     render() {
         return (
-            <div className={"t-main-center"}></div>
+            <div className={"t-main-center"}>
+                <div className={"ui fixed t-main-home"}>Home</div>
+                <div className={"t-post-input"}>
+                    <div className={"t-post-input-holder"}>
+                        <textarea className={"t-txtarea"} id="" rows="3" placeholder={"Whats New?"}></textarea>
+                        <button className={"ui button blue"}>Tweet</button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
